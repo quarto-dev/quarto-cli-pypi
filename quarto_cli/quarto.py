cb735a3ef579368e6ac3eaa9df1f5e1b2759be06
Christophe Dervieux <christophe.dervieux@gmail.com>
2026-06-30 13:46:03 +0200

    fix: exit cleanly on Ctrl+C (#7)

--- quarto_cli/quarto.py
+++ quarto_cli/quarto.py
@@ -4,4 +4,7 @@ import subprocess as sp
 
 def run():
 	command = os.path.join(os.path.dirname(__file__), "bin", "quarto")
-	return sp.call([command] + sys.argv[1:])
+	try:
+		return sp.call([command] + sys.argv[1:])
+	except KeyboardInterrupt:
+		sys.exit(130)
